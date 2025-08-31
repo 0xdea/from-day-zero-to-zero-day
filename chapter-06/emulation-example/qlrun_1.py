8c0cd2186ccb10e45e0cc7d147044ddcaa35e30a
raptor <raptor@0xdeadbeef.info>
2025-08-31 17:44:30 +0200

    Add usage information in first qiling script

--- chapter-06/emulation-example/qlrun_1.py
+++ chapter-06/emulation-example/qlrun_1.py
@@ -1,3 +1,10 @@
+#
+# Usage:
+# $ pipx install qiling===1.4.6
+# $ sudo apt install python3-unicorn # Version: 6.1.0-2build2
+# $ pipx run --spec qiling python qlrun_1.py
+# 
+
 from qiling import Qiling
 from qiling.extensions.coverage import utils as cov_utils
 from qiling.const import QL_VERBOSE
